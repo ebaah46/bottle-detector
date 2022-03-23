d1eeadedbb9a54545bb1112559a3895c2e0b2a45
ebaah46 <ebaah46@yahoo.com>
2022-03-24 01:53:21 +0200

    finds most bottles

--- hm_assignment.py
+++ hm_assignment.py
@@ -1,11 +1,11 @@
 import cv2 as cv
 import os
 from math import sqrt, pi
-from matplotlib.pyplot import contour
+# from matplotlib.pyplot import contour
 import numpy as np
 from os import listdir
 from cv2 import vconcat, imshow
-
+from scipy.spatial import distance as dist
 
 # Path for images
 path = "bottle_crate_images/"
@@ -22,8 +22,10 @@ def component_analysis(image, raw_image):
     # img = image.copy()
     img_filtered = cv.cvtColor(raw_image, cv.COLOR_GRAY2BGR)
     # print(labels)
-    # Counter for component naming
-    count = 1
+
+    count = 1  # Counter for component naming
+    last_marked = None  # Last marked component as a bottle
+    # current_component = None  # The current component being analysed
     for i in range(1, num_of_labels):
         # Extract the statistics of each component
         x = stats[i, cv.CC_STAT_LEFT]
@@ -31,20 +33,34 @@ def component_analysis(image, raw_image):
         w = stats[i, cv.CC_STAT_WIDTH]
         h = stats[i, cv.CC_STAT_HEIGHT]
         area = stats[i, cv.CC_STAT_AREA]
-        cX, cY = centroids[i]
-        if area > 120 and area < 200 and w <= 39 and w >= 35:
-            # Check radius size for all circles
-            radius = sqrt(area / pi)
-            # print(f"Radius for circle with centre:{cX,cY} is:{radius}")
-            # if (radius <= 6.59 and radius >= 6.30):
-            cv.rectangle(img_filtered, (x, y),
-                         (x + w, y + h), (0, 255, 0), 1)
-            cv.putText(img_filtered, str(count), org=(int(cX), int(cY)),
-                       fontFace=cv.FONT_HERSHEY_COMPLEX, fontScale=0.4, color=(255, 255, 255), thickness=1, lineType=cv.LINE_8)
-            count = count + 1
-            print(f"width: {w}")
+        current_component = centroids[i]
+        if area > 120 and area < 250:
+            # and w <= 45 and w >= 35 unused metric for comparing components
+            if h >= 30 and h <= 55 and w >= 30 and w <= 55 and abs(w - h) < 5:
+                if last_marked is None:  # Identifying first component
+
+                    cv.rectangle(img_filtered, (x, y),
+                                 (x + w, y + h), (0, 255, 0), 1)
+                    cv.putText(img_filtered, str(count), org=(int(current_component[0]), int(current_component[1])),
+                               fontFace=cv.FONT_HERSHEY_COMPLEX, fontScale=0.4, color=(255, 0, 0), thickness=1, lineType=cv.LINE_8)
+                    print(
+                        f"centroids:{current_component}\tcount:{count}\twidth: {w}\theight:{h}")
+                else:
+                    # Check the distance between the centre of the current component and the previous component
+                    distance = dist.euclidean(last_marked, current_component)
+                    if distance < 90:
+                        continue
+                    cv.rectangle(img_filtered, (x, y),
+                                 (x + w, y + h), (0, 255, 0), 1)
+                    cv.putText(img_filtered, str(count), org=(int(current_component[0]), int(current_component[1])),
+                               fontFace=cv.FONT_HERSHEY_COMPLEX, fontScale=0.4, color=(255, 0, 0), thickness=1, lineType=cv.LINE_8)
+                    print(
+                        f"count:{count}\tcentroids:{current_component}\twidth: {w}\theight:{h}\tdistance:{distance:.2f}")
+                last_marked = current_component  # Last marked component as a bottle
+                count = count + 1
+
     # Draw rectangle around component
-    cv.imshow("Components", img_filtered)
+    imshow("Components", img_filtered)
     cv.waitKey(0)
 
 
@@ -55,6 +71,7 @@ for index, img in enumerate(img_dir):
     edges = cv.dilate(edges, kernel=np.ones(
         (1, 1), np.uint8))
     component_analysis(edges, img)
+    # exit()
     continue
 
 
@@ -103,4 +120,13 @@ for index, img in enumerate(img_dir):
 4. Find all connected components in the image.
 5. Filter through component properties for desired components.
 6. Label the components in each image.
+
+
+Modifications to make
+1. Check a particular height that fits most of the components.
+    How do I identify the first component that is a bottle.
+2. Figure out the components that have vastly uneven widht and height and remove them.
+3. Find a way to remove all non-bottle components that are identified. One way could be the 
+    euclidean distance between the centre points of the  individual components identified.
+    If the centre points are within a distance closer to each other, ignore the current component
 """
