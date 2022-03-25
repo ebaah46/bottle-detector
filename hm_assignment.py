49ded8374b78290827019111c1a486bc483c8c2e
ebaah46 <ebaah46@yahoo.com>
2022-03-25 07:55:39 +0200

    3 similar appraoches

--- hm_assignment.py
+++ hm_assignment.py
@@ -1,16 +1,9 @@
+from curses import raw
 import cv2 as cv
-import os
-from math import sqrt, pi
-# from matplotlib.pyplot import contour
 import numpy as np
 from os import listdir
-from cv2 import vconcat, imshow
+from cv2 import imshow
 from scipy.spatial import distance as dist
-
-# Path for images
-path = "bottle_crate_images/"
-# Load images directory
-img_dir = listdir(path)
 
 
 # Component analysis function
@@ -18,10 +11,8 @@ def component_analysis(image, raw_image):
     # component analysis with stats
     (num_of_labels, labels, stats, centroids) = cv.connectedComponentsWithStats(
         image, 8, cv.CV_32S)
-    # make copy of provided image
-    # img = image.copy()
+    # convert raw_image to color image to be displayed
     img_filtered = cv.cvtColor(raw_image, cv.COLOR_GRAY2BGR)
-    # print(labels)
 
     count = 1  # Counter for component naming
     last_marked = None  # Last marked component as a bottle
@@ -34,17 +25,16 @@ def component_analysis(image, raw_image):
         h = stats[i, cv.CC_STAT_HEIGHT]
         area = stats[i, cv.CC_STAT_AREA]
         current_component = centroids[i]
-        if area > 120 and area < 250:
-            # and w <= 45 and w >= 35 unused metric for comparing components
+        if area > 120 and area < 250:  # Discard all components outside the desired area values of the bottles
             if h >= 30 and h <= 55 and w >= 30 and w <= 55 and abs(w - h) < 5:
                 if last_marked is None:  # Identifying first component
-
+                    # Draw a rectangle around the component
                     cv.rectangle(img_filtered, (x, y),
                                  (x + w, y + h), (0, 255, 0), 1)
                     cv.putText(img_filtered, str(count), org=(int(current_component[0]), int(current_component[1])),
                                fontFace=cv.FONT_HERSHEY_COMPLEX, fontScale=0.4, color=(255, 0, 0), thickness=1, lineType=cv.LINE_8)
                     print(
-                        f"centroids:{current_component}\tcount:{count}\twidth: {w}\theight:{h}")
+                        f"count:{count}\tcentroids:{current_component}\twidth: {w}\theight:{h}")
                 else:
                     # Check the distance between the centre of the current component and the previous component
                     distance = dist.euclidean(last_marked, current_component)
@@ -59,58 +49,78 @@ def component_analysis(image, raw_image):
                 last_marked = current_component  # Last marked component as a bottle
                 count = count + 1
 
-    # Draw rectangle around component
     imshow("Components", img_filtered)
     cv.waitKey(0)
 
 
-# Loop through images and select each image to find circles
-for index, img in enumerate(img_dir):
-    img = cv.imread(path+img_dir[index], 0)
-    edges = cv.Canny(img, 80, 200, apertureSize=3)
-    edges = cv.dilate(edges, kernel=np.ones(
-        (1, 1), np.uint8))
-    component_analysis(edges, img)
-    # exit()
-    continue
-
-
-# TODO
-# Algorithm for counting bottles in the crate
-
-# Contours approach
-""" 
-    #  Draw contour around the crate
-    contours, _ = cv.findContours(
-        eroded_opened, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    print(f"No. of contours: {len(contours)}")
-    print(f"contour 2: {contours[1]}")
-    for contour in contours:
-        approx = cv.approxPolyDP(
-            contour, 0.1 * cv.arcLength(contour, True), True)
-        x, y, w, h = cv.boundingRect(contour)
-        cv.rectangle(img_bgr, (x, y), (x+w, y+h), (0, 0, 255), 2)
-        cv.drawContours(img_bgr, contour, -1, (0, 255, 0), 3)
-
-"""
-
 # Hough Circles approach
-""" 
+def hough_cirlces(image, raw_image):
+    cv.GaussianBlur(raw_image, (9, 9), 2, raw_image, 2)
     circles = cv.HoughCircles(
-        edges, cv.HOUGH_GRADIENT, dp=1, minDist=80, param1=200, param2=0.95, minRadius=18, maxRadius=20)
+        raw_image, cv.HOUGH_GRADIENT, dp=1, minDist=92, param1=200, param2=0.9, minRadius=17, maxRadius=22)
 
     circles = np.uint16(np.around(circles))
+    img_filtered = cv.cvtColor(raw_image, cv.COLOR_GRAY2BGR)
     print(len(circles[0]))
     for circle in circles[0, :]:
         cv.circle(img_filtered, (circle[0],
                   circle[1]), circle[2], color=(0, 255, 0), thickness=2)
         # print(circle)
-    img_combined = vconcat(
-        [img_bgr, img_filtered])
-    cv.imshow(f"Image {index+1} v Image manipulated", img_combined)
+    imshow(f"Image {index+1} v Image manipulated", img_filtered)
     cv.waitKey(0)
 
-"""
+
+# Contours approach
+def contours(image, raw_image):
+    #  Draw contour around the crate
+    contours, _ = cv.findContours(
+        image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # Convert image to color image
+    raw_image = cv.cvtColor(raw_image, cv.COLOR_GRAY2BGR)
+    for _, cnt in enumerate(contours):
+        # Discard all contours that are too small
+        area = cv.contourArea(cnt)
+        if area < 50:
+            continue
+        # print(area)
+        x, y, w, h = cv.boundingRect(cnt)
+        if h >= 30 and h <= 55 and w >= 30 and w <= 55 and abs(w - h) < 5:
+            print(f"contour:{_+1}\twidth:{w}\theight:{h}")
+            cv.rectangle(raw_image, (x, y), (x+w, y+h), (0, 0, 255), 1)
+        # cv.drawContours(raw_image, cnt, -1, (0, 255, 0), 3)
+    imshow("Image with drawn contours", raw_image)
+    cv.waitKey(0)
+
+
+if "__main__" == __name__:
+
+    # Path for images
+    path = "bottle_crate_images/"
+    # Load images directory
+    img_dir = listdir(path)
+
+    # Loop through images and select each image to find circles
+    for index, img in enumerate(img_dir):
+        # read image into memory
+        img = cv.imread(path+img_dir[index], 0)
+        # Detect edges in the image
+        edges = cv.Canny(img, 80, 200, apertureSize=3)
+        # morphological dilation
+        edges = cv.dilate(edges, kernel=np.ones((1, 1), np.uint8))
+        # component analysis on list of edges
+        # component_analysis(edges, img)
+
+        # Perform hough transform
+        # hough_cirlces(edges, img)
+        # Perfomr contour detection
+        contours(edges, img)
+        continue
+        # exit()
+
+
+# TODO
+# Algorithm for counting bottles in the crate
+
 
 # Connected Components approach
 """
